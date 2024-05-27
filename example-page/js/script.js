33880f97b8c116f6f13832510eb8011562d6a028
Pavlo Krovitskyi <123174653+PavloKrovitskyi@users.noreply.github.com>
2024-05-28 01:43:10 +0200

    Add files via upload

---
+++ example-page/js/script.js
@@ -0,0 +1,9 @@
+document.addEventListener("click", function (e) {
+  const targetElement = e.target;
+
+  if (targetElement.closest('.icon-menu')) {
+    document.documentElement.classList.toggle('menu-open');
+    e.preventDefault();
+  }
+
+});
